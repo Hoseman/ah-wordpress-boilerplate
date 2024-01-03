d78b4e3cdc165a72f85946242f30fd9da05995f2
Andrew Hosegood <you@example.com>
2024-01-03 18:41:27 +0000

    update starter theme files

--- wp-content/themes/ah-wordpress-boilerplate/page.php
+++ wp-content/themes/ah-wordpress-boilerplate/page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<section class="container">
 
 		<?php
 		while ( have_posts() ) :
@@ -31,8 +31,8 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+	</section>
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
